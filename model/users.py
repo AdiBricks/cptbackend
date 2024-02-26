891f017229db5d230ae7b1d5e23482a78c7b4e1e
Aditya Ramesh <adityaadarshramesh@gmail.com>
2024-02-26 20:18:01 +0000

    userspycommit

--- model/users.py
+++ model/users.py
@@ -78,16 +78,18 @@ class User(db.Model):
     _uid = db.Column(db.String(255), unique=True, nullable=False)
     _password = db.Column(db.String(255), unique=False, nullable=False)
     _dob = db.Column(db.Date)
+    _favsubject = db.Column(db.String(255), unique=True, nullable=False)
     
     # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
     posts = db.relationship("Post", cascade='all, delete', backref='users', lazy=True)
 
     # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, password="123qwerty", dob=date.today()):
+    def __init__(self, name, uid, password="123qwerty", dob=date.today(), _favsubject=""):
         self._name = name    # variables with self prefix become part of the object, 
         self._uid = uid
         self.set_password(password)
         self._dob = dob
+        self._favsubject = _favsubject
 
     # a name getter method, extracts name from object
     @property
@@ -144,6 +146,14 @@ class User(db.Model):
         today = date.today()
         return today.year - self._dob.year - ((today.month, today.day) < (self._dob.month, self._dob.day))
     
+    @property
+    def fav_subject(self):
+        return self._fav_subject
+
+    @fav_subject.setter
+    def fav_subject(self, subject):
+        self._fav_subject = subject
+
     # output content using str(object) in human readable form, uses getter
     # output content using json dumps, this is ready for API response
     def __str__(self):
@@ -170,7 +180,9 @@ class User(db.Model):
             "uid": self.uid,
             "dob": self.dob,
             "age": self.age,
+            "favsubject": self.favsubject, 
             "posts": [post.read() for post in self.posts]
+
         }
 
     # CRUD update: updates user name, password, phone
@@ -203,10 +215,10 @@ def initUsers():
         """Create database and tables"""
         db.create_all()
         """Tester data for table"""
-        u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11))
-        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', dob=date(1856, 7, 10))
-        u3 = User(name='Alexander Graham Bell', uid='lex')
-        u4 = User(name='Grace Hopper', uid='hop', password='123hop', dob=date(1906, 12, 9))
+        u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11), _favsubject='csp')
+        u2 = User(name='Nicholas Tesla', uid='niko', password='123niko', dob=date(1856, 7, 10), _favsubject='chem')
+        u3 = User(name='Alexander Graham Bell', uid='lex', _favsubject='chem')
+        u4 = User(name='Grace Hopper', uid='hop', password='123hop', dob=date(1906, 12, 9), _favsubject='physics')
         users = [u1, u2, u3, u4]
 
         """Builds sample user/note(s) data"""
